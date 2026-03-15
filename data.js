8544bae0366a4c13527514764e2d38e0769c3286
obousquet <obousquet@gmail.com>
2026-03-15 22:14:15 +0000

    Add creative categories and adjust styling for long category names

--- data.js
+++ data.js
@@ -3,12 +3,16 @@
 // to eventually create challenging puzzles with overlapping terms.
 
 const wordRepository = {
-    "Fruits": ["Apple", "Banana", "Orange", "Grape", "Kiwi", "Mango", "Pear", "Peach", "Plum", "Berry"],
-    "Colors": ["Red", "Blue", "Green", "Yellow", "Orange", "Purple", "Pink", "Brown", "Black", "White"],
-    "Vehicles": ["Car", "Truck", "Bus", "Motorcycle", "Bicycle", "Train", "Plane", "Boat", "Helicopter", "Scooter"],
-    "Animals": ["Dog", "Cat", "Bird", "Fish", "Elephant", "Tiger", "Bear", "Lion", "Zebra", "Monkey"],
-    "Trees": ["Oak", "Pine", "Maple", "Birch", "Cedar", "Elm", "Willow", "Ash", "Spruce", "Fir"],
-    "Planets": ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
-    "Metals": ["Gold", "Silver", "Copper", "Iron", "Zinc", "Lead", "Platinum", "Aluminum", "Bronze", "Brass"],
-    "Instruments": ["Piano", "Guitar", "Violin", "Drums", "Flute", "Cello", "Trumpet", "Saxophone", "Bass", "Harp"]
+    "Fruits starting with the letter P": ["Peach", "Pear", "Plum", "Papaya", "Pomegranate", "Pineapple", "Persimmon", "Pomelo"],
+    "James Bond characters": ["Bond", "M", "Q", "Moneypenny", "Blofeld", "Oddjob", "Jaws", "Scaramanga"],
+    "Harry Potter characters": ["Harry", "Ron", "Hermione", "Snape", "Dumbledore", "Hagrid", "Sirius", "Draco"],
+    "Soap brands": ["Dove", "Ivory", "Dial", "Zest", "Coast", "Safeguard", "Olay", "Lux"],
+    "Prefixes of car brands": ["Chev", "Volks", "Toyo", "Hon", "Nis", "Mer", "Cad", "Sub"],
+    "Things with a mouse": ["Computer", "Cat", "Trap", "Disney", "Maze", "Cheese", "Pad", "Hole"],
+    "End in 'berry'": ["Straw", "Blue", "Black", "Cran", "Rasp", "Goose", "Elder", "Mul"],
+    "Starts with 'Sun'": ["Flower", "Glasses", "Light", "Dial", "Set", "Rise", "Shine", "Burn"],
+    "Palindromes": ["Racecar", "Level", "Radar", "Civic", "Kayak", "Madam", "Rotor", "Stats"],
+    "Words that sound like letters": ["Sea", "Eye", "Jay", "Cue", "Are", "Tea", "You", "Why"],
+    "Things you can play": ["Piano", "Game", "Music", "Fool", "Catch", "Role", "Guitar", "Sports"],
+    "Synonyms for Happy": ["Glad", "Joyful", "Cheerful", "Merry", "Jolly", "Upbeat", "Thrilled", "Elated"]
 };
